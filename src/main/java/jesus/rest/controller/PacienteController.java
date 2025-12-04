e109b5f90542fadec081fb9721ac5b43a00330bb
Your Name <you@example.com>
2025-12-04 13:27:17 +0100

    a

--- src/main/java/jesus/rest/controller/PacienteController.java
+++ src/main/java/jesus/rest/controller/PacienteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
+
 import jesus.rest.dto.PacienteDTO;
 import jesus.rest.service.PacienteService;
 
--- src/main/java/jesus/rest/dto/PacienteDTO.java
+++ src/main/java/jesus/rest/dto/PacienteDTO.java
@@ -1,11 +1,10 @@
 package jesus.rest.dto;
 
-import javax.validation.constraints.*;
+import jakarta.validation.constraints.*;
 
 public record PacienteDTO(
-        Long id, 
-        @NotBlank @Size(max = 60) String nombre,
-        @Size(max = 255) String antecedentes) {
-
+                Long id,
+                @NotBlank @Size(max = 60) String nombre,
+                @Size(max = 255) String antecedentes) {
 
 }
